33805b8ae379588acf78b01018dd1a5edc7103fd
uni2tw <uni2tw@gmail.com>
2019-10-20 01:03:21 +0800

    minor

--- WhatSearch/build.bat
+++ WhatSearch/build.bat
@@ -1,7 +1,6 @@
-rem dotnet publish -r win-x64 -c Release
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
 robocopy C:\Git2019\WhatSearch\WhatSearch\bin\Release\netcoreapp3.1\win-x64\publish C:\Git2019\WhatSearch_publish /MIR
 del C:\Git2019\WhatSearch_publish\config.json
 del C:\Git2019\WhatSearch_publish\Log4Net.config
 robocopy C:\Git2019\WhatSearch\contents C:\Git2019\WhatSearch_publish\contents /MIR
-timeout 20
+timeout 5
