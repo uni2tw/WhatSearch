69af85f10919f02af85a937aa37e0bbf3315a081
uni2tw <uni2tw@gmail.com>
2020-06-02 00:34:46 +0800

    更新版號到0.3.4 私人空間上傳

--- WhatSearch/Controllers/UploadController.cs
+++ WhatSearch/Controllers/UploadController.cs
@@ -4,16 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using WhatSearch.Core;
 using WhatSearch.Utility;
 
 namespace WhatSearch.Controllers
 {
-    //[Route("[controller]")]
     public class UploadController : Controller
     {
         
-        SystemConfig config = Ioc.GetConfig();
+        static SystemConfig config = Ioc.GetConfig();
 
         public long LimitMb
         {
@@ -23,33 +23,120 @@ namespace WhatSearch.Controllers
             }
         }
 
+        static UploadController()
+        {
+            //10分鐘清一次
+            System.Timers.Timer timer = new System.Timers.Timer(1000 * 60 * 10);
+            timer.AutoReset = false;            
+            timer.Elapsed += delegate (object sender, System.Timers.ElapsedEventArgs e)
+            {
+                try
+                {
+                    RemoveOldFiles();
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    (sender as System.Timers.Timer).Start();
+                }
+            };
+            timer.Start();
+        }
+
+        public static void RemoveOldFiles()
+        {
+            DateTime now = DateTime.Now;
+            {
+                DirectoryInfo diTemp = GetWorkTempFolder(null);
+                foreach (var fsi in diTemp.GetFileSystemInfos())
+                {
+                    var di2 = fsi as DirectoryInfo;
+                    if (di2 != null)
+                    {
+                        foreach (var fi2 in di2.GetFiles())
+                        {
+                            TimeSpan deleteAfter = TimeSpan.FromHours(4) - (now - fi2.LastWriteTime);
+                            if (deleteAfter.TotalSeconds <= 0)
+                            {
+                                fi2.Delete();
+                            }
+                        }
+                        if (di2.GetFiles().Length == 0)
+                        {
+                            di2.Delete();
+                        }
+                    }
+                    else
+                    {
+                        TimeSpan deleteAfter = TimeSpan.FromHours(4) - (now - fsi.LastWriteTime);
+                        if (deleteAfter.TotalSeconds <= 0)
+                        {
+                            fsi.Delete();
+                        }
+                    }
+                }
+            }
+
+
+            {
+                DirectoryInfo diTemp = GetWorkFolder(null);
+                foreach (var fsi in diTemp.GetFileSystemInfos())
+                {
+                    var di2 = fsi as DirectoryInfo;
+                    if (di2 != null)
+                    {
+                        foreach (var fi2 in di2.GetFiles())
+                        {
+                            TimeSpan deleteAfter = TimeSpan.FromDays(3) - (now - fi2.LastWriteTime);
+                            if (deleteAfter.TotalSeconds <= 0)
+                            {
+                                fi2.Delete();
+                            }
+                        }
+                        if (di2.GetFiles().Length == 0)
+                        {
+                            di2.Delete();
+                        }
+                    }
+                    else
+                    {
+                        TimeSpan deleteAfter = TimeSpan.FromHours(4) - (now - fsi.LastWriteTime);
+                        if (deleteAfter.TotalSeconds <= 0)
+                        {
+                            fsi.Delete();
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Route("{secret}/upload")]
         [Route("upload")]
-        public IActionResult List()
+        public IActionResult List([FromRoute]Guid? secret)
         {
             string errorMessage;
             if (IsEnabled(out errorMessage) == false)
             {
                 return Content(errorMessage);
             }
-            string uploadPath = config.Upload.Folder;
-            DirectoryInfo di = new DirectoryInfo(uploadPath);
-            List<FileDownloadInfoModel> files = new List<FileDownloadInfoModel>();
+
+            DirectoryInfo di = GetWorkFolder(secret);
             DateTime now = DateTime.Now;
 
+            List<FileInfo> fiInfos = new List<FileInfo>();
+            if (di.Exists)
             {
-                DirectoryInfo diTemp = new DirectoryInfo(config.Upload.TempFolder);
-                foreach (var fi in diTemp.GetFiles())
-                {
-                    TimeSpan deleteAfter = TimeSpan.FromHours(4) - (now - fi.LastWriteTime);
-                    if (deleteAfter.TotalSeconds <= 0)
-                    {
-                        fi.Delete();
-                        continue;
-                    }
-                }
+                fiInfos.AddRange(di.GetFiles().OrderByDescending(t => t.CreationTime));
             }
-
-            foreach (var fi in di.GetFiles().OrderByDescending(t => t.CreationTime))
+            List<FileDownloadInfoModel> files = new List<FileDownloadInfoModel>();
+            foreach (var fi in fiInfos)
             {
                 TimeSpan deleteAfter = TimeSpan.FromDays(3) - (now - fi.LastWriteTime);
                 if (deleteAfter.TotalSeconds <= 0)
@@ -61,22 +148,62 @@ namespace WhatSearch.Controllers
                 {
                     Title = fi.Name,
                     Id = fi.Name,
-                    //Size =  fi.Length.ToString("N0"),
                     Size = Helper.GetReadableByteSize(fi.Length),
                     Time = fi.CreationTime.ToString("yyyy-MM-dd HH:mm:ss"),
                     DeleteAfter = Helper.GetReadableTimeSpan(deleteAfter)
                 });
             }
+            
             ViewBag.LimitMb = LimitMb;
+            ViewBag.Secret = secret?.ToString();
             ViewBag.Items = files;
-         
+
             return View();
         }
+
+        private static DirectoryInfo GetWorkFolder(Guid? secret)
+        {
+            if (secret == null)
+            {
+                return new DirectoryInfo(config.Upload.Folder);
+            }
+            string path = Path.Combine(config.Upload.Folder, secret.ToString());
+            return new DirectoryInfo(path);
+        }
+
+        private static DirectoryInfo GetWorkTempFolder(Guid? secret)
+        {
+            if (secret == null)
+            {
+                return new DirectoryInfo(config.Upload.TempFolder); 
+            }
+            string path=Path.Combine(config.Upload.TempFolder, secret.ToString());
+            return new DirectoryInfo(path);
+        }
+
+        [HttpGet]
+        [Route("upload/secret")]
+        public IActionResult CreateSecret()
+        {
+            return Redirect(string.Format("/{0}/upload", Guid.NewGuid().ToString()));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file_name"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("upload/status")]
-        public dynamic Status(string file_name)
+        [Route("upload/{secret}/status")]
+        public dynamic Status(string file_name, [FromRoute]Guid? secret)
         {
-            string filePath = Path.Combine(config.Upload.TempFolder, file_name);
+            var tempFolder = GetWorkTempFolder(secret);
+            if (tempFolder.Exists == false)
+            {
+                tempFolder.Create();
+            }
+            string filePath = Path.Combine(tempFolder.FullName, file_name);            
             if (System.IO.File.Exists(filePath))
             {
                 return Ok(new
@@ -87,7 +214,12 @@ namespace WhatSearch.Controllers
                     message = string.Format("先前上傳的檔案 {0} 未完成，你想要續傳嗎 ?", file_name)
                 });
             }
-            filePath = Path.Combine(config.Upload.Folder, file_name);
+            var workFolder = GetWorkFolder(secret);
+            if (workFolder.Exists == false)
+            {
+                workFolder.Create();
+            }
+            filePath = Path.Combine(workFolder.FullName, file_name);
             if (System.IO.File.Exists(filePath))
             {
                 return Ok(new
@@ -114,7 +246,9 @@ namespace WhatSearch.Controllers
         /// <returns></returns>
         [HttpPost]
         [Route("upload/post")]
-        public dynamic PostFile(IFormFile file, bool is_start, bool is_end, string file_name)
+        [Route("upload/{secret}/post")]
+        public dynamic PostFile(IFormFile file, bool is_start, bool is_end, string file_name,
+            [FromRoute] Guid? secret)
         {
             string errorMessage;
             if (IsEnabled(out errorMessage) == false)
@@ -123,7 +257,7 @@ namespace WhatSearch.Controllers
             }
             try
             {
-                string filePath = Path.Combine(config.Upload.TempFolder, file_name);
+                string filePath = Path.Combine(GetWorkTempFolder(secret).FullName, file_name);
                 FileStream fs;
                 if (is_start)
                 {
@@ -150,7 +284,7 @@ namespace WhatSearch.Controllers
                 }
                 if (is_end)
                 {
-                    string destFilePath = Path.Combine(config.Upload.Folder, file_name);
+                    string destFilePath = Path.Combine(GetWorkFolder(secret).FullName, file_name);
                     try
                     {
                         System.IO.File.Move(filePath, destFilePath, true);
@@ -176,15 +310,16 @@ namespace WhatSearch.Controllers
         }
 
         [HttpGet]
+        [Route("upload/{secret}/file/{*pathInfo}")]
         [Route("upload/file/{*pathInfo}")]
-        public dynamic GetUploadFile(string pathInfo)
+        public dynamic GetUploadFile(string pathInfo, Guid? secret)
         {
             string errorMessage;
             if (IsEnabled(out errorMessage) == false)
             {
                 return Content(errorMessage);
             }
-            string targetPath = Path.Combine(config.Upload.Folder, pathInfo);
+            string targetPath = Path.Combine(GetWorkFolder(secret).FullName, pathInfo);
             if (System.IO.File.Exists(targetPath) == false)
             {
                 return NotFound();
