e336867f776db5ba70902bf99c82353f4fe07c4b
uni2tw <uni2tw@gmail.com>
2021-02-08 10:45:34 +0800

    merge images backup

---
+++ WhatSearch/Controllers/MergeImagesController.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using log4net.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using WhatSearch.Core;
+using WhatSearch.Utility;
+
+namespace WhatSearch.Controllers
+{
+    public class MergeImagesController : Controller
+    {
+        
+        static SystemConfig config = Ioc.GetConfig();
+        static ILog logger = LogManager.GetLogger(typeof(MergeImagesController));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>        
+        [Route("mergeImages")]
+        public IActionResult List([FromRoute]Guid? secret)
+        {
+           
+
+            return View();
+        }
+        
+
+    }
+}
